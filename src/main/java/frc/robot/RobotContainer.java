23af6332724ebbf5d9429e22b8703212956a8042
AdaTomruk <adatomruk10@gmail.com>
2026-01-09 19:42:23 +0300

    Refactor DriveToPointCommand execution to defer command generation until 'B' is pressed, ensuring proper drivetrain requirements.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -129,14 +130,17 @@ public class RobotContainer {
 
         //go to point command takes ppoint from elastic dashboard
         joystick.b().whileTrue(
-            driveToPointCommandFactory.generateCommand(
-                new edu.wpi.first.math.geometry.Pose2d(
+            Commands.defer(() -> {
+                // This code now runs ONLY when 'B' is pressed
+                return driveToPointCommandFactory.generateCommand(
+                    new edu.wpi.first.math.geometry.Pose2d(
                     Meters.of(SmartDashboard.getNumber("PointX", 0.0)),
                     Meters.of(SmartDashboard.getNumber("PointY", 0.0)),
                     new Rotation2d(SmartDashboard.getNumber("PointRotation", 0.0))
-                )
             )
         );
+    }, java.util.Set.of(drivetrain)) // Require the drivetrain for this deferred command
+);
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
